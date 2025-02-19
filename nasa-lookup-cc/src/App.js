545854b10b8e8a0c6920b584d41cba64b779b800
Collins Ovuakporaye <thecodexgrey@gmail.com>
2025-02-19 08:29:11 +0200

    Testing: fetching assets from API endpoint

--- nasa-lookup-cc/src/App.js
+++ nasa-lookup-cc/src/App.js
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Search from "./Search";
-import AssetDetail from "./AssetDetail";
+import Search from "./components/Search";
+import AssetDetail from "./components/AssetDetail";
 
 const App = () => {
-    // state to keep track of the selected asset
-    const [selectedAsset, setSelectedAsset] = useState(null);
+    /*
+      fetching assets dynamically in AssetDetail.js, removed selectedAsset. 
+      - state to keep track of the selected asset
+      const [selectedAsset, setSelectedAsset] = useState(null); 
+    */
 
     return (
         <Router>
             <Routes>
                 {/* route: search page, passing a function to update selected asset */}
-                <Route
-                  path="/"
-                  element={<Search onSelectAsset={setSelectedAsset} />}
-                />
+                <Route path="/" element={<Search />} />                 {/* onSelectAsset={setSelectedAsset} */} 
                 
                 {/* route: asset details page, passing the selected asset */}
-                <Route
-                  path="/asset/:id"
-                  element={<AssetDetail asset={selectedAsset} />}
-                />
+                <Route path="/asset/:id" element={<AssetDetail />}  />  {/* asset={selectedAsset}  */}
             </Routes>
         </Router>
     );
